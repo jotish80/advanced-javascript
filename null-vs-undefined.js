c464961ae6e6adce7c32f641787417e3eae6d87c
jotish80 <sjotishchandra@gmail.com>
2020-08-07 10:08:37 +0200

    null-vs-undefined inteview question set

---
+++ null-vs-undefined.js
@@ -0,0 +1,28 @@
+let pakhi;
+console.log(pakhi);
+pakhir variable assign kora hoy nai ba declare kora hoy nai so eita UNDEFINED.
+function add (num1, num2){
+    console.log(num1 + num2);
+    return 
+}
+function call na korle undefined hoye jai ba return likhe kichu return na chaile undefined hoye jai.
+function add (num1, num2){
+     console.log(num1, num2);   
+}
+const result = add (13,?);
+console.log(result);
+function er parameter na call korle undefined dekha jai.
+
+const premik ={ name: 'smart dude', phone: 4532145};
+console.log(premik.gf?);
+emon ekta kisu access korte chao ja tumi declear koro nai tai undefined
+
+let fun = undefined;
+console.log(fun);
+jodi tumi already declear kore diso je let = undefined so eita undefined dekhabe
+
+let ages [2, 3, 9, 10];
+console.log(ages [11]?)
+array te tumi 11 parameter declare koro nai so eit undefiend
+
+Null : holo ekta value already chilo kintu tumi seita ekhon ar access korte parchona karon seita ekhon ar nai
