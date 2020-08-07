ef7c453e8467364ba8f3165bdcb03f78838836ba
jotish80 <sjotishchandra@gmail.com>
2020-08-07 09:49:36 +0200

    true-false

---
+++ true-false.js
@@ -0,0 +1,18 @@
+const name = '0';
+
+if(name){
+    console.log('condition is true');
+}
+else {
+    console.log('condition is false');
+}
+//falsy value:
+// emty string false
+//undefined variable is false
+// 0
+// ''
+// NaN
+// Null
+// false
+//truthy:
+// '0', ' ', [],
